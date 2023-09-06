6cc75c98a7d36749513438a8c968b1aaec98bbf5
tach200 <dev.tom404@gmail.com>
2023-09-06 14:29:55 +0100

    add flag provider code

---
+++ my_provider/provider.go
@@ -0,0 +1,80 @@
+package provider
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/open-feature/go-sdk/pkg/openfeature"
+	"gopkg.in/yaml.v3"
+)
+
+type FeatureProvider struct{}
+
+func lookupYamlVal() (map[interface{}]interface{}, error) {
+	featuresYmlFilePath := "my_provider/flags.yml"
+
+	data, err := os.ReadFile(featuresYmlFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	featureMap := make(map[interface{}]interface{})
+
+	err = yaml.Unmarshal(data, &featureMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return featureMap, nil
+}
+
+func (fp FeatureProvider) BooleanEvaluation(ctx context.Context, flag string, defaultValue bool, evalCtx openfeature.FlattenedContext) openfeature.BoolResolutionDetail {
+
+	featureMap, err := lookupYamlVal()
+	if err != nil {
+		return openfeature.BoolResolutionDetail{
+			Value: defaultValue,
+		}
+	}
+
+	boolValue := featureMap[flag].(bool)
+
+	return openfeature.BoolResolutionDetail{Value: boolValue}
+}
+
+func (fp FeatureProvider) IntEvaluation(ctx context.Context, flag string, defaultValue int64, evalCtx openfeature.FlattenedContext) openfeature.IntResolutionDetail {
+
+	featureMap, err := lookupYamlVal()
+	if err != nil {
+		return openfeature.IntResolutionDetail{
+			Value: defaultValue,
+		}
+	}
+
+	// weird conversion here
+	intValue := featureMap[flag].(int)
+	int64Value := int64(intValue)
+
+	return openfeature.IntResolutionDetail{Value: int64Value}
+}
+
+func (fp FeatureProvider) FloatEvaluation(ctx context.Context, flag string, defaultValue float64, evalCtx openfeature.FlattenedContext) openfeature.FloatResolutionDetail {
+	return openfeature.FloatResolutionDetail{}
+}
+
+func (fp FeatureProvider) Hooks() []openfeature.Hook {
+	return []openfeature.Hook{}
+}
+
+func (fp FeatureProvider) Metadata() openfeature.Metadata {
+	return openfeature.Metadata{}
+}
+
+func (fp FeatureProvider) ObjectEvaluation(context.Context, string, interface{}, openfeature.FlattenedContext) openfeature.InterfaceResolutionDetail {
+	return openfeature.InterfaceResolutionDetail{}
+}
+
+func (fp FeatureProvider) StringEvaluation(context.Context, string, string, openfeature.FlattenedContext) openfeature.StringResolutionDetail {
+	return openfeature.StringResolutionDetail{}
+}
